b2a6a2f11a0a44e3ee8778ac3b125e1851b0b102
Rob <rm.rf.etc@gmail.com>
2019-07-27 16:10:15 -0700

    Add debouncing server-side, and set search field on start

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,13 @@ export const setSearch = (payload) => ({
 	type: actionTypes.SET_SEARCH,
 	payload,
 });
+
+export const threadsLoaded = (payload) => ({
+	type: actionTypes.THREAD_DATA_LOADED,
+	payload,
+});
+
+export const userDataLoaded = (payload) => ({
+	type: actionTypes.USER_DATA_LOADED,
+	payload,
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,20 @@ import actionTypes from '../actions/types';
 export default (state = initialState, action = {}) => {
 
   switch (action.type) {
+  	case actionTypes.USER_DATA_LOADED: {
+  		return {
+  			...state,
+  			statusSearchActive: false,
+  			searchString: action.payload.searchString,
+  		};
+  	}
   	case actionTypes.SET_SEARCH: {
   		return {
   			...state,
   			statusSearchActive: true,
   			searchString: action.payload.searchString,
-  		}
+  		};
   	}
-    default: return initialState;
+    default: return state;
   }
 }
