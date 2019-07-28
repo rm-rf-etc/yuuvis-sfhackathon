4cfe5d86b7966a36aad07f9714254185b14367ab
johngarcia9110 <johngarcia9110@gmail.com>
2019-07-27 18:26:24 -0700

    tweaking summary events

--- src/components/john-components/Raw.js
+++ src/components/john-components/Raw.js
@@ -10,10 +10,29 @@ const Div = styled.div`
 	overflow: hidden;
 	overflow-y: scroll;
 	border-top: 1px solid gray;
-	.active{
+	background: gray;
+	.raw-inner{
+	    padding-bottom: 400px;
+	}
+	
+`;
+
+const Email = styled.div`
+    padding: 15px;
+    background: white;
+    margin-bottom: 20px;
+    border-radius: 4px;
+    opacity: .6;
+    transition: all 300ms ease-in-out;
+    &.active{
+	    opacity: 1;
+	}
+	.highlight{
 	    background: yellow;
 	}
+	
 `;
+
 
 const RawDoc = () => {
 
@@ -21,38 +40,38 @@ const RawDoc = () => {
         <Div id="rawContainer">
             <div className="raw-inner">
                 <h1>RAW View</h1>
-                <div id="email1">
+                <Email id="email1">
                     <p>From: User1</p>
                     <p>To: User2</p>
                     <p>Subject: Please approve my vacation request</p>
                     <p id="rawDoc">
                         EMAIL 1 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.
                     </p>
-                </div>
-                <div id="email2">
+                </Email>
+                <Email id="email2">
                     <p>From: User1</p>
                     <p>To: User2</p>
                     <p>Subject: Please approve my vacation request</p>
                     <p id="rawDoc">
                         EMAIL 2 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.
                     </p>
-                </div>
-                <div id="email3">
+                </Email>
+                <Email id="email3">
                     <p>From: User1</p>
                     <p>To: User2</p>
                     <p>Subject: Please approve my vacation request</p>
                     <p id="rawDoc">
                         EMAIL 3 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.
                     </p>
-                </div>
-                <div id="email4">
+                </Email>
+                <Email id="email4">
                     <p>From: User1</p>
                     <p>To: User2</p>
                     <p>Subject: Please approve my vacation request</p>
                     <p id="rawDoc">
                         EMAIL 4 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.
                     </p>
-                </div>
+                </Email>
             </div>
         </Div>
     );
--- src/components/john-components/Summary.js
+++ src/components/john-components/Summary.js
@@ -1,32 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 
 const Div = styled.div`
 	display: flex;
-	text-align: center;
+	text-align: left;
 	flex-direction: column;
+	ul{
+	    margin: 0;
+	    padding: 0;
+	    list-style: none;
+	    border-top: 1px solid gray;
+	    border-bottom: 1px solid gray;
+	    li{
+	        border-bottom: 1px solid gray;
+	        padding: 15px;
+	        background: white;
+	        transition: all 100ms ease-in-out;
+	        &.activeSum{
+	            background: gray;
+	        }
+	        :last-child{
+	            border: 0;
+	        }
+	        p{
+	            background: white;
+	            padding: 5px;
+	            border-radius: 2px;
+	            &.activeSingleSum{
+	                background: yellow;
+	            }
+	        }
+	    }
+	}
 `;
 
-const Highlight = styled.span`
-    background: yellow;
-`;
 
-const findLocationOfString = (strID) => {
-    const needle = document.getElementById(strID).innerHTML;
-    const rawDoc = document.getElementById("rawDoc");
-    rawDoc.innerHTML  = rawDoc.innerHTML.replace(needle, '<div class="highlight"><a id='+strID+'"-loc"></a>'+needle+'</div>');
-    console.log("finished");
+const highlightString = (sumID, emailID) => {
+    const singleSum = document.getElementById('sum' +sumID);
+    const needle = singleSum.innerHTML;
+    const rawDoc = document.getElementById("email"+emailID);
+
+    singleSum.classList.add('activeSingleSum');
+    rawDoc.innerHTML  = rawDoc.innerHTML.replace(needle, '<span class="highlight">'+needle+'</span>');
+}
+
+const removeStringHighlight = (sumID,emailID) => {
+    const singleSum = document.getElementById('sum' +sumID);
+    const activeSingleSum = document.getElementsByClassName('activeSingleSum')[0];
+    const needle = singleSum.innerHTML;
+    const rawDoc = document.getElementById("email"+emailID);
+    if(activeSingleSum){
+        activeSingleSum.classList.remove('activeSingleSum');
+    }
+
+    rawDoc.innerHTML  = rawDoc.innerHTML.replace('<span class="highlight">'+needle+'</span>', needle);
 }
 
 const scrollToEmail  = (emailID) => {
-    const singleEmail = document.getElementById(emailID);
-    const activeEmails = document.getElementsByClassName('active')[0];
-    if(activeEmails){
-        activeEmails.classList.remove('active');
+    const singleEmail = document.getElementById('email' + emailID);
+    const singleSummaryGroup = document.getElementById('sumGroup'+emailID);
+    const activeEmail = document.getElementsByClassName('active')[0];
+    const activeSum = document.getElementsByClassName('activeSum')[0];
+    if(activeEmail){
+        activeEmail.classList.remove('active');
     }
+    if(activeSum){
+        activeSum.classList.remove('activeSum');
+    }
+    singleSummaryGroup.classList.add('activeSum');
     singleEmail.classList.add('active');
-    singleEmail.scrollIntoView();
+    singleEmail.scrollIntoView({behavior: "smooth", block: "start", inline: "center"});
 }
 
 const Summary = () => {
@@ -34,16 +78,27 @@ const Summary = () => {
         <Div>
             <h1>Thread Summaries</h1>
             <ul>
-                <li onClick={()=>scrollToEmail("email1")}>
-                    <p id="sentence1">The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water.</p>
+                <li id="sumGroup1" onClick={()=>scrollToEmail("1")}>
+                    <p id="sum1"
+                       onMouseEnter={() => highlightString("1", "1")}
+                       onMouseOut={() => removeStringHighlight("1","1")}
+                    >at an alarming rate</p>
+                    <p id="sum2"
+                       onMouseEnter={() => highlightString("2","1")}
+                       onMouseOut={() => removeStringHighlight("2","1")}
+                    >The consequences are proving to be profound</p>
+                    <p id="sum3"
+                       onMouseEnter={() => highlightString("3","1")}
+                       onMouseOut={() => removeStringHighlight("3","1")}
+                    >aquifers dry up</p>
                 </li>
-                <li onClick={()=>scrollToEmail("email2")}>
+                <li id="sumGroup2" onClick={()=>scrollToEmail("2")}>
                     <p id="sentence2">Up to 75% of farmers rely on pumped groundwater to water their crops and water use is intensifying – at the same time that satellite images shows supplies are shrinking alarmingly.</p>
                 </li>
-                <li onClick={()=>scrollToEmail("email3")}>
+                <li id="sumGroup3"  onClick={()=>scrollToEmail("3")}>
                     <p id="sentence3">The nature of the problem is revealed by US Geological Survey figures, which show that the total amount of fresh water on Earth comes to about 10.6m cubic km.</p>
                 </li>
-                <li onClick={()=>scrollToEmail("email4")}>
+                <li id="sumGroup4"  onClick={()=>scrollToEmail("4")}>
                     <p id="sentence4">By contrast, the total volume from lakes and rivers, humanity’s main source of fresh water, produces a sphere that is a mere 56 km in diameter.</p>
                 </li>
             </ul>
