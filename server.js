0e07325e867c28209d765435ef4d3655c11ece9c
Rob <rm.rf.etc@gmail.com>
2019-07-27 20:51:36 -0700

    Change to multi-summary

--- server.js
+++ server.js
@@ -31,12 +31,26 @@ const seeds = {
     name: 'Bob Dylan',
     searchString: '',
   },
+  'summary/1': {
+    textValue: `Up to 75% of farmers rely on pumped groundwater to water their crops and water use is intensifying – at the same time that satellite images shows supplies are shrinking alarmingly.`,
+  },
+  'summary/2': {
+    textValue: `Up to 75% of farmers rely on pumped groundwater to water their crops and water use is intensifying – at the same time that satellite images shows supplies are shrinking alarmingly.`,
+  },
+  'summary/3': {
+    textValue: `The nature of the problem is revealed by US Geological Survey figures, which show that the total amount of fresh water on Earth comes to about 10.6m cubic km.`,
+  },
+  'summary/4': {
+    textValue: `By contrast, the total volume from lakes and rivers, humanity’s main source of fresh water, produces a sphere that is a mere 56 km in diameter.`,
+  },
+  'summary/5': {
+    textValue: `By contrast, the total volume from lakes and rivers, humanity’s main source of fresh water, produces a sphere that is a mere 56 km in diameter.`,
+  },
   'email/1': {
     from: 'user/1',
     to: 'user/2',
     subject: 'Please approve my vacation request',
     body: `EMAIL 1 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.`,
-    summary: `Up to 75% of farmers rely on pumped groundwater to water their crops and water use is intensifying – at the same time that satellite images shows supplies are shrinking alarmingly.`,
     notes: ['at an alarming rate', 'The consequences are proving to be profound', 'aquifers dry up'],
   },
   'email/2': {
@@ -44,28 +58,24 @@ const seeds = {
     to: 'user/2',
     subject: 'Please approve my vacation request',
     body: `EMAIL 2 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.`,
-    summary: `Up to 75% of farmers rely on pumped groundwater to water their crops and water use is intensifying – at the same time that satellite images shows supplies are shrinking alarmingly.`,
   },
   'email/3': {
     from: 'user/1',
     to: 'user/2',
     subject: 'Please approve my vacation request',
     body: `EMAIL 3 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.`,
-    summary: `The nature of the problem is revealed by US Geological Survey figures, which show that the total amount of fresh water on Earth comes to about 10.6m cubic km.`,
   },
   'email/4': {
     from: 'user/1',
     to: 'user/2',
     subject: 'Please approve my vacation request',
     body: `EMAIL 4 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.`,
-    summary: `By contrast, the total volume from lakes and rivers, humanity’s main source of fresh water, produces a sphere that is a mere 56 km in diameter.`,
   },
   'email/5': {
     from: 'user/1',
     to: 'user/2',
     subject: 'Please approve my vacation request',
     body: `EMAIL 5 Water is the driving force of all nature, Leonardo da Vinci claimed. Unfortunately for our planet, supplies are now running dry – at an alarming rate. The world’s population continues to soar but that rise in numbers has not been matched by an accompanying increase in supplies of fresh water. The consequences are proving to be profound. Across the globe, reports reveal huge areas in crisis today as reservoirs and aquifers dry up.`,
-    summary: `By contrast, the total volume from lakes and rivers, humanity’s main source of fresh water, produces a sphere that is a mere 56 km in diameter.`,
   },
 };
 const attachments = [
@@ -74,6 +84,11 @@ const attachments = [
   ['thread/2', 'email/3'],
   ['thread/2', 'email/4'],
   ['thread/2', 'email/5'],
+  ['email/1.summaries', 'summary/1'],
+  ['email/2.summaries', 'summary/2'],
+  ['email/3.summaries', 'summary/3'],
+  ['email/4.summaries', 'summary/4'],
+  ['email/5.summaries', 'summary/5'],
   ['user/1.threads', 'thread/1'],
   ['user/1.threads', 'thread/2'],
 ];
@@ -94,23 +109,24 @@ gun.path('user/1.searchString').on(searchHandler);
 
 var textapi = new AYLIENTextAPI({
   application_id: "8e9d825d",
-  application_key: "d515b564aa74cd8ccfeb10ffb2e8a99c"
+  application_key: "d515b564aa74cd8ccfeb10ffb2e8a99c",
 });
 
 async function summarize(email) {
-    let summaries = email;
-    textapi.summarize({
-	'text': email,
-	'title': 'placeholder'
-    }, function(error, response) {
-	if (error === null) {
-	    console.log(response);
-	    if (response.sentences.length === 0) {
-		response.sentences.push(text);
-	    }
-	    summaries = response.sentences;
-	}
-    });
-    return summaries;
+  let summaries = email;
+  textapi.summarize({
+    'text': email,
+    'title': 'placeholder'
+  },
+  function(error, response) {
+    if (error === null) {
+      console.log(response);
+      if (response.sentences.length === 0) {
+        response.sentences.push(text);
+      }
+      summaries = response.sentences;
+    }
+  });
+  return summaries;
 }
 
